7630540404162872070ad560f31a0cac89f39784
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-09-12 14:51:38 -0400

    fix assignment 2

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounter.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/WordCounter.java
@@ -10,23 +10,19 @@ import java.io.IOException;
 import java.io.BufferedReader;
 
 public class WordCounter {
-    // checks if word is less than
     public static boolean isTrivial(String word) {
         return word.length() <= 3;
     }
 
-    // checks is its a numeric string
     public static boolean isNumeric(String str) {
         return str.matches("\\d+");
     }
 
     public static void main(String[] args) {
-        // Map to hold counts
+        // Map to help hold counts
         Map<String, Integer> wordCounts = new HashMap<>();
 
-        // File name (I put this in the base directory)
-        String fileName = "words.txt";
-
+        String fileName = "LSP_Fall_2024/src/org/howard/edu/lsp/assignment2/words.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -44,7 +40,7 @@ public class WordCounter {
             System.out.println(e.getMessage());
         }
 
-        // Display output
+        // output
         for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
             System.out.printf("%-10s %d%n", entry.getKey(), entry.getValue());
         }
