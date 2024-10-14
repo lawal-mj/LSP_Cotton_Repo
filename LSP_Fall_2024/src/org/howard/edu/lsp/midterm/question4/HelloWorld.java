e641dbb39469259b6603a4edc5a9d4d1ea83383d
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2024-10-14 17:20:55 -0400

    midterm changes

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question4/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*  Name: Abdulmujeeb Lawal
+*/
+package org.howard.edu.lsp.midterm.question4;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
